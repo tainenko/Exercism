b76c9396cb00f233203cc5718f378172eef871f0
ko tainen <TonyK@hyvesolutions.com>
2020-05-29 01:07:28 +0800

    add Solution of gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -6,13 +6,15 @@
 package gigasecond
 
 // import path for the time package from the standard library
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	tEpoch := float64(t.Unix())
+	gigaSecond := math.Pow(10.0, 9)
+	tPlusGigaSecond := tEpoch + gigaSecond
+	return time.Unix(int64(tPlusGigaSecond), 0)
 }
