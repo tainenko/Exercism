ee1f629300495d5f6416b6e3e4afff72d8b9dc32
ko tainen <TonyK@hyvesolutions.com>
2020-07-15 03:23:33 +0800

    Implement the Distance of hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,18 @@
+// Package hamming provide a method to calculate the Hamming Distance
 package hamming
 
+import "errors"
+
+// Distance function return a humming distance of two DNA string.
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("the Hamming distance is only defined for sequences of equal length")
+	}
+	var count int
+	for i := range a {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count, nil
 }
