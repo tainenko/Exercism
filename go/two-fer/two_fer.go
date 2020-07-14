f9b2cea44949290f1e724244a617ad47dd3c4d12
ko tainen <TonyK@hyvesolutions.com>
2020-07-15 01:55:13 +0800

    fix syntax for golint

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,12 +1,9 @@
-// Package twofer
-/*
-Twofer package provide a function which return a formatted sentence.
-*/
+// Package twofer provides a function which return a formatted sentence.
 package twofer
 
 import "fmt"
 
-// Sharewith accepts a name variable of type string.
+// ShareWith accepts a name variable of type string.
 // Returns a string in the form "One for %s, One for me."
 // If the name is an empty string, return "One for you, One for me."
 func ShareWith(name string) string {
