d9eca7d5a4c70297bc3ed3122829f9c62aa8a1f1
ko tainen <TonyK@hyvesolutions.com>
2020-07-15 17:00:21 +0800

    refactor the logic of Conver

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -8,20 +8,17 @@ import "strconv"
 // if the number is not a multiple of 3 or 5 or 7, return the string of number.
 // if the number has [3, 5, 7] as a factor, add ['Pling', 'Plang', 'Plong'] to the result
 func Convert(num int) (result string) {
-	if 0 != num%3 && 0 != num%5 && 0 != num%7 {
-		return strconv.Itoa(num)
-	}
 	if num%3 == 0 {
 		result += "Pling"
-		num /= 3
 	}
 	if num%5 == 0 {
 		result += "Plang"
-		num /= 5
 	}
 	if num%7 == 0 {
 		result += "Plong"
-		num /= 7
+	}
+	if result == "" {
+		result = strconv.Itoa(num)
 	}
 	return
 }
