35cf43d8e5008cf8eb8881c918ff0781a6c1c144
ko tainen <TonyK@hyvesolutions.com>
2020-07-16 23:01:42 +0800

    remove redundant code

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,8 +1,5 @@
 class TwoFer {
-    public static twoFer(name: string = ""): string {
-        if (name == "") {
-            name = "you"
-        }
+    public static twoFer(name: string = "you"): string {
         return `One for ${name}, one for me.`
     }
 }
