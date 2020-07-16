bb1d299f9d6736451191b6915e0d7efa1d3eacb3
ko tainen <TonyK@hyvesolutions.com>
2020-07-16 16:04:41 +0800

    Add annotations of Score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,6 +2,7 @@ package scrabble
 
 import "strings"
 
+//Score function takes a string as input and returns the sum of each runes in the string.
 func Score(input string) (score int) {
 	scoreMap := map[rune]int{
 		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
