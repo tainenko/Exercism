ef40c75f6f40025f23e0696c0cfc13073e33267e
ko tainen <TonyK@hyvesolutions.com>
2020-07-19 01:03:05 +0800

    Add solution of isogram.go

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,22 @@
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram takes a string as input and return whether the input is Isogram.
+// definition of isogram is there are no duplicate letters.
+func IsIsogram(input string) bool {
+	set := make(map[rune]bool)
+	for _, val := range strings.ToLower(input) {
+		if !unicode.IsLetter(val) {
+			continue
+		}
+		if _, ok := set[val]; ok {
+			return false
+		}
+		set[val] = true
+	}
+	return true
+}
