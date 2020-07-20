345fada963086504de321a54c23c27d330758c26
ko tainen <TonyK@hyvesolutions.com>
2020-07-20 14:07:21 +0800

    Add solution of leetcode reverse_string.go

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,1 +1,9 @@
 package reverse
+
+// Reverse would return a reversed string of input
+func Reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
