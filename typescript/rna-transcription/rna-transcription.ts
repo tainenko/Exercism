8dbf340c4065dfa00ecd968d9c9dd9993ab4b376
ko tainen <TonyK@hyvesolutions.com>
2020-07-22 16:30:55 +0800

    add solution of rna-transcription.ts

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,6 +1,23 @@
+const dna2Rna = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U"
+}
+type DNA = keyof typeof dna2Rna
+
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    toRna(dna: string): string {
+        let res = ""
+        for (let i = 0; i < dna.length; i++) {
+            let rna = dna2Rna[dna[i] as DNA] || this.raiseInvalidInputError()
+            res += rna
+        }
+        return res
+    }
+
+    raiseInvalidInputError() {
+        throw new Error("Invalid input DNA.")
     }
 }
 
