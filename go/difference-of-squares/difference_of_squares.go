b3dd4829fe9e31f5f9dcfc15529bb2c0d093d2bd
ko tainen <TonyK@hyvesolutions.com>
2020-07-26 01:46:01 +0800

    remove abs()

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,7 +1,5 @@
 package diffsquares
 
-import "math"
-
 // SquareOfSum takes a int as input and return the square of sum.
 // formula: 1+2+3+...+n=n*(n+1)/2
 func SquareOfSum(n int) (result int) {
@@ -15,12 +13,7 @@ func SumOfSquares(n int) (result int) {
 	return n * (n + 1) * (2*n + 1) / 6
 }
 
-// Difference function would return the absolute difference between SquareOfSum and SumOfSquares
-func Difference(target int) int {
-	return abs(SumOfSquares(target) - SquareOfSum(target))
-
-}
-
-func abs(n int) int {
-	return int(math.Abs(float64(n)))
+// Difference function would return the  difference between SquareOfSum and SumOfSquares
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
 }
