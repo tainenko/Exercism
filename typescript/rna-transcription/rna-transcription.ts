fc0030df22feceedb6e07ed40f6803cbb17cf767
ko tainen <TonyK@hyvesolutions.com>
2020-07-26 11:17:42 +0800

    remove type cast

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,23 +1,24 @@
-const dna2Rna = {
-    "G": "C",
-    "C": "G",
-    "T": "A",
-    "A": "U"
-}
-type DNA = keyof typeof dna2Rna
-
 class Transcriptor {
     toRna(dna: string): string {
-        let res = ""
-        for (let i = 0; i < dna.length; i++) {
-            let rna = dna2Rna[dna[i] as DNA] || this.raiseInvalidInputError()
-            res += rna
-        }
-        return res
+        return dna.split('').map(this.convertNucleotide).join('')
     }
 
-    raiseInvalidInputError() {
-        throw new Error("Invalid input DNA.")
+    convertNucleotide(strand: string): string {
+        switch (strand) {
+            case "G":
+                return "C"
+            case "C":
+                return "G"
+            case "T":
+                return "A"
+            case "A":
+                return "U"
+            default:
+                throw new Error("Invalid input DNA.")
+
+        }
+
+
     }
 }
 
