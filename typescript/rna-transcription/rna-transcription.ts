98c0ae06f668a6ffd9ee22b74bd1de1a0a2676e6
ko tainen <TonyK@hyvesolutions.com>
2020-07-30 23:12:13 +0800

    add type guard

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,24 +1,30 @@
+type NucleotidesOfDna = "G" | "C" | "T" | "A"
+type NucleotidesOfRna = "C" | "G" | "A" | "U"
+const NucleotidesMap: Record<string, NucleotidesOfRna> = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U"
+}
+
 class Transcriptor {
-    toRna(dna: string): string {
+    toRna = (dna: string): string => {
         return dna.split('').map(this.convertNucleotide).join('')
     }
 
-    convertNucleotide(strand: string): string {
-        switch (strand) {
-            case "G":
-                return "C"
-            case "C":
-                return "G"
-            case "T":
-                return "A"
-            case "A":
-                return "U"
-            default:
-                throw new Error("Invalid input DNA.")
-
+    convertNucleotide = (strand: string): NucleotidesOfRna => {
+        if (!this.isNucleotidesOfDna(strand)) {
+            this.raiseInvalidInputError()
         }
+        return NucleotidesMap[strand]
+    }
 
+    isNucleotidesOfDna = (strand: string): strand is NucleotidesOfDna => {
+        return NucleotidesMap[strand] !== undefined
+    }
 
+    raiseInvalidInputError = (): void => {
+        throw new Error("Invalid input DNA.")
     }
 }
 
