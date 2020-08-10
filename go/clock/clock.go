c008f6957b2e31e9a9ec73304682b6a7d13eb25a
ko tainen <TonyK@hyvesolutions.com>
2020-08-10 09:17:23 +0800

    implement Clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,1 +1,35 @@
 package clock
+
+type Clock struct {
+	hour   int
+	minute int
+}
+
+func New(hour, minute int) *Clock {
+	c := new(Clock)
+	c.hour = hour
+	c.minute = minute
+	return c
+}
+
+func (c *Clock) String() string {
+	return ""
+}
+
+func (c *Clock) Add(minutes int) *Clock {
+	c.minute += minutes
+	if c.minute >= 60 {
+		c.minute -= 60
+		c.hour += 1
+	}
+	return c
+}
+
+func (c *Clock) Subtract(minutes int) *Clock {
+	c.minute -= minutes
+	if c.minute < 0 {
+		c.minute += 60
+		c.hour -= 1
+	}
+	return c
+}
