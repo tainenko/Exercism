fde84a87f2f1b67a7b0f29f883d0ead165b8da1f
ko tainen <TonyK@hyvesolutions.com>
2020-08-10 08:55:13 +0800

    simplify the solution of uint64 operation

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,6 @@
 package grains
 
-import (
-	"errors"
-	"math"
-)
+import "errors"
 
 // Square function calculate the number of grains of wheat on a chessboard given that the number
 // on each square doubles.
@@ -11,11 +8,10 @@ func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, errors.New("square must be greater than zero or less than or equal to 64")
 	}
-	return uint64(math.Pow(2, float64(n-1))), nil
+	return uint64(1) << (n - 1), nil
 }
 
-// Total return the value of 1+2+4+8+...+2**64
+// Total return the max value of uint64
 func Total() uint64 {
-	// formula: 1+2+4+8+...+2**64=2**65-1
-	return 2*uint64(math.Pow(2, float64(64))) - 1
+	return ^uint64(0)
 }
