c16f25c62b4800c392402b9b89708727c0b25bf3
ko tainen <TonyK@hyvesolutions.com>
2020-08-15 12:35:09 +0800

    add solution of bob.ts

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,6 +1,31 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+
+    hey(speech: string): string {
+        if (this.isNothing(speech)) {
+            return 'Fine. Be that way!'
+        }
+        if (this.isQuestion(speech) && this.isYelling(speech)) {
+            return 'Calm down, I know what I\'m doing!'
+        }
+        if (this.isYelling(speech)) {
+            return 'Whoa, chill out!'
+        }
+        if (this.isQuestion(speech)) {
+            return 'Sure.'
+        }
+        return 'Whatever.'
+    }
+
+    private isQuestion(speech: string): boolean {
+        return speech.trimEnd().endsWith("?")
+    }
+
+    private isYelling(speech: string): boolean {
+        return /[A-Z]/.test(speech) && speech === speech.toUpperCase()
+    }
+
+    private isNothing(speech: string): boolean {
+        return speech.length === 0 || /^\s*$/.test(speech)
     }
 }
 
