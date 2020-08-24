9a72bb0fbf3b69df16b42ac2f16e9a6e7a056e9e
ko tainen <TonyK@hyvesolutions.com>
2020-08-24 12:11:45 +0800

    Add the solution of parallel_letter_frequency.go

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,14 +1,34 @@
 package letter
 
-// FreqMap records the frequency of each rune in a given text.
+// FreqMap is a type of map[rune]int
 type FreqMap map[rune]int
 
-// Frequency counts the frequency of each rune in a given text and returns this
-// data as a FreqMap.
+// Frequency counts the rune frequency in a string
+// and return a FreqMap Object
 func Frequency(s string) FreqMap {
-	m := FreqMap{}
-	for _, r := range s {
-		m[r]++
+	freqMap := FreqMap{}
+	for _, c := range s {
+		freqMap[c] ++
 	}
-	return m
+	return freqMap
+}
+
+// ConcurrentFrequency receive a string array and call the Frequency function
+// with goroutine
+func ConcurrentFrequency(StringArray []string) FreqMap {
+	ch := make(chan FreqMap, len(StringArray))
+	for _, s := range StringArray {
+		s := s
+		go func() {
+			ch <- Frequency(s)
+		}()
+	}
+
+	freqMap := FreqMap{}
+	for range StringArray {
+		for k, v := range <-ch {
+			freqMap[k] += v
+		}
+	}
+	return freqMap
 }
