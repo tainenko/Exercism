27cb404af4b96b43ab38ea8a8e5b7556298effa1
ko tainen <TonyK@hyvesolutions.com>
2021-04-15 12:03:46 +0800

    implemented resistor-color.ts

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,28 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
+const colorMap: Record<string, number> = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+export const colorCode = (color: string): number => {
+    return colorMap[color]
 }
 
-export const COLORS = undefined
+export const COLORS: string[] = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+]
