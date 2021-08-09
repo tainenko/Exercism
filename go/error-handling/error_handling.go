6acc52377a6f826394c60a1349d7e20060570401
ko tainen <TonyK@hyvesolutions.com>
2021-08-09 22:40:48 +0800

    add solution of exercism error_handling.go

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,1 +1,24 @@
 package erratum
+
+//Use func opens a resource, calls Frob(input) on the result resource and then closes that resource (in all cases).
+func Use(o ResourceOpener, input string) (err error) {
+	var res Resource
+
+	for res, err = o(); err != nil; res, err = o() {
+		if _, ok := err.(TransientError); !ok {
+			return err
+		}
+	}
+	defer res.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			if fe, ok := r.(FrobError); ok{
+				res.Defrob(fe.defrobTag)
+			}
+			err = r.(error)
+		}
+	}()
+
+	res.Frob(input)
+	return err
+}
