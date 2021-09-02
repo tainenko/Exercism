538ea15c39a206c51edbcda72b8d95979b3de64a
ko tainen <TonyK@hyvesolutions.com>
2021-09-02 09:58:46 +0800

    add solution of lasagna.go

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -1,9 +1,26 @@
 package lasagna
 
-// TODO: define the 'OvenTime()' function
+// OvenTime function that does not take any parameters and returns how many minutes the lasagna should be in the oven
+func OvenTime() int {
+	return 40
+}
 
-// TODO: define the 'RemainingOvenTime()' function
+// RemainingOvenTime function that takes the actual minutes the lasagna has been in the oven as a parameter and returns
+// how many minutes the lasagna still has to remain in the oven, based on the expected oven time in minutes from the
+// previous task.
+func RemainingOvenTime(number int) int {
+	return OvenTime() - number
+}
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime function that takes the number of layers you added to the lasagna as a parameter and returns how many
+// minutes you spent preparing the lasagna, assuming each layer takes you 2 minutes to prepare.
+func PreparationTime(layer int) int {
+	preparingTimeInMinute := 2
+	return layer * preparingTimeInMinute
+}
 
-// TODO: define the 'ElapsedTime()' function
+// ElapsedTime function that takes two parameters: the first parameter is the number of layers you added to the lasagna,
+// and the second parameter is the number of minutes the lasagna has been in the oven.
+func ElapsedTime(layer, minute int) int {
+	return PreparationTime(layer) + minute
+}
