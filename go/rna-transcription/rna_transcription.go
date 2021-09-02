4c325133b0ef3b4399f0ab1f0b41cc56a13ce6b0
ko tainen <TonyK@hyvesolutions.com>
2021-09-02 20:25:42 +0800

    add solution of rna_transcription.go

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,18 @@
 package strand
 
+var toRnaMap = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA convert input dna string and return a rna string
 func ToRNA(dna string) string {
+	rna := make([]rune, len(dna))
+	for k, r := range dna {
+		rna[k] = toRnaMap[r]
+	}
+	return string(rna)
+
 }
