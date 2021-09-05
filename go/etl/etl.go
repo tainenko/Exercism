647a18e15e943b86b5f673e93aeea8f1923e3c7c
ko tainen <TonyK@hyvesolutions.com>
2021-09-05 23:56:01 +0800

    add solution of etl.go

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,1 +1,16 @@
 package etl
+
+import "strings"
+
+// Transform the legacy data format to the shiny new format
+// The old system stored a list of letters per score
+// The shiny system stores a list of score per the letters in lower-case regardless of the case of the input letters.
+func Transform(m map[int][]string) map[string]int {
+	var res = make(map[string]int)
+	for k, v := range m {
+		for _, s := range v {
+			res[strings.ToLower(s)] = k
+		}
+	}
+	return res
+}
