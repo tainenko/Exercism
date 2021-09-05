20fe935485f32e5803c7e9ad9f787481e49290c8
ko tainen <TonyK@hyvesolutions.com>
2021-09-06 00:21:59 +0800

    add solution of scrabble_score.dart

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,36 @@
-// Put your code here
+var scoreMap = {
+  "a": 1,
+  "e": 1,
+  "i": 1,
+  "o": 1,
+  "u": 1,
+  "l": 1,
+  "n": 1,
+  "r": 1,
+  "s": 1,
+  "t": 1,
+  "d": 2,
+  "g": 2,
+  "b": 3,
+  "c": 3,
+  "m": 3,
+  "p": 3,
+  "f": 4,
+  "h": 4,
+  "v": 4,
+  "w": 4,
+  "y": 4,
+  "k": 5,
+  "j": 8,
+  "x": 8,
+  "q": 10,
+  "z": 10,
+};
+
+int score(letter) {
+  var res = 0;
+  for (var i = 0; i < letter.length; i++) {
+    res += scoreMap[letter[i].toLowerCase()] ?? 0;
+  }
+  return res;
+}
--- dart/scrabble-score/test/scrabble_score_test.dart
+++ dart/scrabble-score/test/scrabble_score_test.dart
@@ -11,51 +11,51 @@ void main() {
     test('uppercase letter', () {
       final int result = score('A');
       expect(result, equals(1));
-    }, skip: true);
+    }, skip: false);
 
     test('valuable letter', () {
       final int result = score('f');
       expect(result, equals(4));
-    }, skip: true);
+    }, skip: false);
 
     test('short word', () {
       final int result = score('at');
       expect(result, equals(2));
-    }, skip: true);
+    }, skip: false);
 
     test('short, valuable word', () {
       final int result = score('zoo');
       expect(result, equals(12));
-    }, skip: true);
+    }, skip: false);
 
     test('medium word', () {
       final int result = score('street');
       expect(result, equals(6));
-    }, skip: true);
+    }, skip: false);
 
     test('medium, valuable word', () {
       final int result = score('quirky');
       expect(result, equals(22));
-    }, skip: true);
+    }, skip: false);
 
     test('long, mixed-case word', () {
       final int result = score('OxyphenButazone');
       expect(result, equals(41));
-    }, skip: true);
+    }, skip: false);
 
     test('english-like word', () {
       final int result = score('pinata');
       expect(result, equals(8));
-    }, skip: true);
+    }, skip: false);
 
     test('empty input', () {
       final int result = score('');
       expect(result, equals(0));
-    }, skip: true);
+    }, skip: false);
 
     test('entire alphabet available', () {
       final int result = score('abcdefghijklmnopqrstuvwxyz');
       expect(result, equals(87));
-    }, skip: true);
+    }, skip: false);
   });
 }
