7d9cb830ed4862e163956fa1a91d46daab9cfca2
ko tainen <TonyK@hyvesolutions.com>
2021-09-08 23:18:58 +0800

    add solution of gross_store.go

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -2,25 +2,52 @@ package gross
 
 // Units store the Gross Store unit measurement
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728}
 }
 
 // NewBill create a new bill
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	return map[string]int{}
 }
 
 // AddItem add item to customer bill
-func AddItem(bill map[string]int, item string, unit string) bool {
-	panic("Please implement the AddItem() function")
+func AddItem(bill map[string]int, units map[string]int, item string, unit string) bool {
+	if _, ok := units[unit]; !ok {
+		return false
+	}
+	bill[item] += units[unit]
+	return true
 }
 
 // RemoveItem remove item from customer bill
-func RemoveItem(bill map[string]int, item string, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+func RemoveItem(bill map[string]int, units map[string]int, item string, unit string) bool {
+	if _, ok := bill[item]; !ok {
+		return false
+	}
+	if _, ok := units[unit]; !ok {
+		return false
+	}
+	newQuatity := bill[item] - units[unit]
+	if newQuatity < 0 {
+		return false
+	} else if newQuatity == 0 {
+		delete(bill, item)
+	} else {
+		bill[item] -= units[unit]
+	}
+	return true
 }
 
 // GetItem return the quantity of item that the customer has in his/her bill
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	if _, ok := bill[item]; !ok {
+		return 0, false
+	}
+	return bill[item], true
 }
