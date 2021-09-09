46a07f5057d9c2c20ddd254ccd9e7760424643de
ko tainen <TonyK@hyvesolutions.com>
2021-09-09 22:59:28 +0800

    add solution of card_tricks.go

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -3,21 +3,35 @@ package cards
 // GetItem retrieves an item from a slice at given position. The second return value indicates whether
 // a the given index existed in the slice or not.
 func GetItem(slice []uint8, index int) (uint8, bool) {
-	panic("Please implement the GetItem function")
+	if index >= len(slice) || index < 0 {
+		return 0, false
+	}
+	return slice[index], true
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range it is be appended.
 func SetItem(slice []uint8, index int, value uint8) []uint8 {
-	panic("Please implement the SetItem function")
+	if index >= len(slice) || index < 0 {
+		return append(slice, value)
+	}
+	slice[index] = value
+	return slice
 }
 
 // PrefilledSlice creates a slice of given length and prefills it with the given value.
 func PrefilledSlice(value, length int) []int {
-	panic("Please implement the PrefilledSlice function")
+	var slice []int
+	for i := 0; i < length; i++ {
+		slice = append(slice, value)
+	}
+	return slice
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if index >= len(slice) || index < 0 {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
 }
