4519def0e446ff38bdae3a0d589acb4944b88b02
ko tainen <TonyK@hyvesolutions.com>
2021-09-14 23:50:52 +0800

    add solution of pangram.go

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,1 +1,14 @@
 package pangram
+
+import "unicode"
+
+// IsPangram return true if input string contains every letter of the alphabet
+func IsPangram(s string) bool {
+	var m = map[rune]int{}
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			m[unicode.ToLower(r)]++
+		}
+	}
+	return len(m) == 26
+}
