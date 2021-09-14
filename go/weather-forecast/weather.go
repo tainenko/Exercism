1b6b424c9c0d54b93f0c64bed8ee911e23b3066d
ko tainen <TonyK@hyvesolutions.com>
2021-09-15 00:31:14 +0800

    add solution of weather.go

--- go/weather-forecast/weather.go
+++ go/weather-forecast/weather.go
@@ -1,10 +1,14 @@
+// Package weather provides a Log function to record the weather condition of a specific location
 package weather
 
 var (
+	// CurrentCondition represents the weather condition
 	CurrentCondition string
-	CurrentLocation  string
+	// CurrentLocation represents the location
+	CurrentLocation string
 )
 
+// Log is the record of weather condition in a specific location
 func Log(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
