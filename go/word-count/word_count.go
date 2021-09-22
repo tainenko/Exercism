2a9bae775ede169f89e241737789abe93c853332
ko tainen <TonyK@hyvesolutions.com>
2021-09-23 00:31:45 +0800

    add solution of word_count.go

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,1 +1,21 @@
 package wordcount
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Frequency is type of map
+type Frequency map[string]int
+
+var re = regexp.MustCompile(`\w+('\w)*`)
+
+// WordCount will return the frequency of each word in phrase
+func WordCount(phrase string) Frequency {
+	phrase = strings.ToLower(phrase)
+	f := make(Frequency)
+	for _, word := range re.FindAllString(phrase, -1) {
+		f[word]++
+	}
+	return f
+}
