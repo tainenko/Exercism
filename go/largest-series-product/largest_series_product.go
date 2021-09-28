4f386b825b97be6e4c83f896c9ac9bff3ea0f8cc
ko tainen <TonyK@hyvesolutions.com>
2021-09-28 23:59:18 +0800

    add solution of largest_series_product.go

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,1 +1,53 @@
 package lsproduct
+
+import "errors"
+
+// LargestSeriesProduct would return the largest series n product in given string
+func LargestSeriesProduct(s string, n int) (int, error) {
+	if len(s) < n {
+		return -1, errors.New("string is shorter than span")
+	}
+
+	if n < 0 {
+		return -1, errors.New("negative span")
+	}
+
+	if n == 0 {
+		return 1, nil
+	}
+
+	tmp := 1
+	cnt := 0
+	var result int
+	for i := 0; i < len(s); {
+		if s[i] < '0' || s[i] > '9' {
+			return -1, errors.New("input contains alpha-characters")
+		}
+
+		if s[i] == '0' {
+			i++
+			tmp = 1
+			cnt = 0
+			continue
+		}
+		tmp *= int(s[i] - '0')
+		cnt++
+		if cnt > n {
+			tmp /= int(s[i-n] - '0')
+			cnt--
+		}
+		result = max(result, tmp)
+		i++
+	}
+	if cnt == n {
+		return result, nil
+	}
+	return 0, nil
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
