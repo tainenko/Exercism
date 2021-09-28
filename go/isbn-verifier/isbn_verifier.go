9609b3f0604b0e5a7f572dac4a7bca4fdcd42155
ko tainen <TonyK@hyvesolutions.com>
2021-09-28 23:01:33 +0800

    add solution of isbn_verifier.go

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,1 +1,25 @@
 package isbn
+
+import (
+	"regexp"
+	"strings"
+)
+
+var re = regexp.MustCompile("^[0-9]{9}[0-9X]$")
+
+// IsValidISBN would check if the input string is valid.
+func IsValidISBN(isbn string) bool {
+	isbn = strings.Replace(isbn, "-", "", -1)
+	if !re.MatchString(isbn) {
+		return false
+	}
+	sum := 0
+	for i := 0; i < 10; i++ {
+		if isbn[i] == 'X' {
+			sum += (10 - i) * 10
+		} else {
+			sum += (10 - i) * int(isbn[i]-'0')
+		}
+	}
+	return sum%11 == 0
+}
