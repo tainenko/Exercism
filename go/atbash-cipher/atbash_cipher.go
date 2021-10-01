08cf14450adea1279cd2a384a91c1a3d208da3ea
ko tainen <TonyK@hyvesolutions.com>
2021-10-01 23:31:29 +0800

    add solution of atbash_cipher.go

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,1 +1,31 @@
 package atbash
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Atbash encrypt the original string and return
+func Atbash(plain string) string {
+	var cipher []rune
+	for _, r := range plain {
+
+		if r >= 'a' && r <= 'z' {
+			r = rune(25 - int(r) + 'a' + 'a')
+		} else if r >= 'A' && r <= 'Z' {
+			r = rune(25 - int(r) + 'A' + 'a')
+		} else if r >= '0' && r <= '9' {
+		} else {
+			continue
+		}
+		cipher = append(cipher, r)
+	}
+
+	return strings.TrimSpace(insertNewLines(string(cipher), 5))
+}
+
+func insertNewLines(s string, n int) string {
+	var r = regexp.MustCompile("(.{" + strconv.Itoa(n) + "})")
+	return r.ReplaceAllString(s, "$1 ")
+}
