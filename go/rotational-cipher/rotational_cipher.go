fb667b7dddf061967f148cb4368925a1e722765d
ko tainen <TonyK@hyvesolutions.com>
2021-10-01 22:55:35 +0800

    add solution of rotational_cipher.go

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,1 +1,23 @@
 package rotationalcipher
+
+// RotationalCipher would shift the input plain and return
+func RotationalCipher(plain string, shiftKey int) string {
+	shiftKey %= 26
+	if shiftKey == 0 {
+		return plain
+	}
+	var shifted []rune
+	var newChr rune
+	for _, chr := range plain {
+		if chr >= 'a' && chr <= 'z' {
+			newChr = rune((int(chr)-'a'+shiftKey)%26) + 'a'
+		} else if chr >= 'A' && chr <= 'Z' {
+			newChr = rune((int(chr)-'A'+shiftKey)%26) + 'A'
+		} else {
+			newChr = chr
+		}
+		shifted = append(shifted, newChr)
+	}
+	return string(shifted)
+
+}
