66d6a9f2e8f4b66e25e1408e475dca90552032fc
ko tainen <TonyK@hyvesolutions.com>
2021-10-03 19:18:22 +0800

    add solution of lasagna-master.go

--- go/lasagna-master/lasagna-master.go
+++ go/lasagna-master/lasagna-master.go
@@ -1,9 +1,45 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime return the estimate for the total preparation time based on the number of layers as an int.
+func PreparationTime(layers []string, time int) int {
+	if time == 0 {
+		time = 2
+	}
+	return len(layers) * time
+}
 
-// TODO: define the 'Quantities()' function
+const quantityOfNoodles = 50
+const quantityOfSauce = 0.2
 
-// TODO: define the 'AddSecretIngredient()' function
+// Quantities determines the quantity of noodles and sauce needed to make your meal.
+func Quantities(layers []string) (int, float64) {
+	var quantitiesOfNoodles int
+	var quantitiesOfSauce float64
+	for _, layer := range layers {
+		if layer == "noodles" {
+			quantitiesOfNoodles += quantityOfNoodles
+		}
+		if layer == "sauce" {
+			quantitiesOfSauce += quantityOfSauce
+		}
+	}
+	return quantitiesOfNoodles, quantitiesOfSauce
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// AddSecretIngredient unction should generate a new slice and add the last item from your
+// friends list to the end of your list.
+func AddSecretIngredient(friendsList, mylist []string) []string {
+	return append(mylist, friendsList[len(friendsList)-1])
+}
+
+const portionOfSlice = 2.0
+
+// ScaleRecipe return a slice of float64s with the amounts needed for the desired number of portions.
+func ScaleRecipe(quantities []float64, portion int) []float64 {
+	var amounts []float64
+	scale := float64(portion) / portionOfSlice
+	for _, quantity := range quantities {
+		amounts = append(amounts, quantity*scale)
+	}
+	return amounts
+}
