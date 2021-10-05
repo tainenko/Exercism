7ba52849bee1ae309a5a349e6e80f29d1cbbd64f
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-10-06 00:20:32 +0800

    add solution of nth_prime.go

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,5 +1,28 @@
 package prime
 
+import "math"
+
+// Nth returns nth prime
 func Nth(n int) (int, bool) {
-	panic("Implement the func for it to return Nth prime number")
+	if n < 1 {
+		return 0, false
+	}
+	cnt := 0
+	for i := 2; ; i++ {
+		if isPrime(i) {
+			cnt++
+		}
+		if cnt == n {
+			return i, true
+		}
+	}
+}
+
+func isPrime(n int) bool {
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
