14c5e6bf426154c8b4edeaaa196a07b2cec49834
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-10-09 14:00:22 +0800

    add solution of prime_factors.go

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,1 +1,13 @@
 package prime
+
+// Factors compute the prime factors of a given natural number.
+func Factors(n int64) []int64 {
+	primes := []int64{}
+	for i := int64(2); i <= n; i++ {
+		for n%i == 0 {
+			primes = append(primes, i)
+			n /= i
+		}
+	}
+	return primes
+}
