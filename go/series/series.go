a7928e29fd98f0198f2f7f9559dfd29c2a7bd473
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-10-09 19:05:23 +0800

    add solution of series.go

--- go/series/series.go
+++ go/series/series.go
@@ -1,1 +1,15 @@
 package series
+
+// All  returns a list of all substrings of s with length n
+func All(n int, s string) []string {
+	var out []string
+	for i := 0; i < len(s)-n+1; i++ {
+		out = append(out, s[i:i+n])
+	}
+	return out
+}
+
+// UnsafeFirst returns the first substring of s with length n
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
+}
