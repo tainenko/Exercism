6ea923c913079c5a29423d209b4b251b6ca97dc5
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-10-14 23:52:31 +0800

    add solution of savings_account.go

--- go/interest-is-interesting/savings_account.go
+++ go/interest-is-interesting/savings_account.go
@@ -1,21 +1,39 @@
 package savings
 
+import (
+	"math"
+)
+
 // InterestRate calculates the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	if balance < 0 {
+		return -3.213
+	}
+	if balance < 1000 {
+		return 0.5
+	}
+	if balance < 5000 {
+		return 1.621
+	}
+	return 2.475
 }
 
-// InterestRate calculates the interest for the provided balance.
+// Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * math.Abs(float64(InterestRate(balance))) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	for i := 1; ; i++ {
+		balance = AnnualBalanceUpdate(balance)
+		if balance > targetBalance {
+			return i
+		}
+	}
 }
