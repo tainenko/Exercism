61ecfff73227d13e9438f3da3237397366876fd7
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-04 22:20:17 +0800

    add solution of collatz_conjecture.go

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,23 @@
 package collatzconjecture
 
+import "errors"
+
+// CollatzConjecture given a number n, return the number of steps required to reach 1.
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n == 0 {
+		return 0, errors.New("n can't be zero")
+	}
+	if n < 0 {
+		return 0, errors.New("n can't be negative")
+	}
+	step := 0
+	for n != 1 {
+		step++
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	return step, nil
 }
