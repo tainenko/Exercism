7f2f7c6713f418b30e9271d995408308e62e8c11
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-05 00:40:40 +0800

    add solution of list_ops.go

--- go/list-ops/list_ops.go
+++ go/list-ops/list_ops.go
@@ -3,34 +3,75 @@ package listops
 // IntList is an abstraction of a list of integers which we can define methods on
 type IntList []int
 
-func (s IntList) Foldl(fn func(int) bool, initial int) int {
-	panic("Please implement the Foldl function")
+type binFunc func(int, int) int
+type predFunc func(int) bool
+type unaryFunc func(int) int
+
+// Foldl given a function, a list, and initial accumulator, fold (reduce) each item into the accumulator from the left
+// using function(accumulator, item)
+func (s IntList) Foldl(fn binFunc, initial int) int {
+	for _, v := range s {
+		initial = fn(initial, v)
+	}
+	return initial
 }
 
-func (s IntList) Foldr(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+// Foldr given a function, a list, and an initial accumulator, fold (reduce) each item into the accumulator from the
+// right using function(item, accumulator)
+func (s IntList) Foldr(fn binFunc, initial int) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		initial = fn(s[i], initial)
+	}
+	return initial
 }
 
+// Filter given a predicate and a list, return the list of all items for which predicate(item) is True
 func (s IntList) Filter(fn func(int) bool) IntList {
-	panic("Please implement the Filter function")
+	newLst := IntList{}
+	for _, v := range s {
+		if fn(v) {
+			newLst = append(newLst, v)
+		}
+	}
+	return newLst
 }
 
+// Length given a list, return the total number of items within it
 func (s IntList) Length() int {
-	panic("Please implement the Length function")
+	return len(s)
 }
 
+// Map given a function and a list, return the list of the results of applying function(item) on all items
 func (s IntList) Map(fn func(int) int) IntList {
-	panic("Please implement the Map function")
+	for i := range s {
+		s[i] = fn(s[i])
+	}
+	return s
 }
 
+// Reverse given a list, return a list with all the original items, but in reversed order
 func (s IntList) Reverse() IntList {
-	panic("Please implement the Reverse function")
+	if len(s) == 0 {
+		return s
+	}
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+	return s
 }
 
+// Append given two lists, add all items in the second list to the end of the first list
 func (s IntList) Append(lst IntList) IntList {
-	panic("Please implement the Append function")
+	return append(s, lst...)
 }
 
+// Concat given a series of lists, combine all items in all lists into one flattened list
 func (s IntList) Concat(lists []IntList) IntList {
-	panic("Please implement the Concat function")
+	for _, lst := range lists {
+		s = s.Append(lst)
+	}
+	return s
 }
