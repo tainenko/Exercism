c89a3a15f63a782a214698fa877a3db83fc07121
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-04 22:38:14 +0800

    add solution of darts.go

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,18 @@
 package darts
 
+import "math"
+
+// Score given a point in the target (defined by its real cartesian coordinates x and y),
+// returns the correct amount earned by a dart landing in that point.
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	dist := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+	if dist <= 1 {
+		return 10
+	} else if dist <= 5 {
+		return 5
+	} else if dist <= 10 {
+		return 1
+	} else {
+		return 0
+	}
 }
