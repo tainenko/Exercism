3f38d877ebc26bdc408e26dcefd87853df706741
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-04 22:58:40 +0800

    add solution of acronym.go

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -5,11 +5,23 @@
 // https://golang.org/doc/effective_go.html#commentary
 package acronym
 
+import (
+	"regexp"
+	"strings"
+)
+
+var reg = regexp.MustCompile(`([ _-]+)`)
+
 // Abbreviate should have a comment documenting it.
 func Abbreviate(s string) string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	return ""
+	words := reg.Split(s, -1)
+	var tla strings.Builder
+	for _, word := range words {
+		tla.WriteString(strings.ToUpper(word[:1]))
+	}
+	return tla.String()
 }
