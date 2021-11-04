4efcda544219d130757014efc5ea1944e1769b3e
Tony Ko <31752048+tainenko@users.noreply.github.com>
2021-11-04 08:03:02 +0800

    add solution of flatten_array.go

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,5 +1,22 @@
 package flatten
 
+// Flatten accepts an arbitrarily-deep nested list-like structure and returns a flattened structure without any nil/null values.
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	flatten := []interface{}{}
+	nestedSlice, ok := nested.([]interface{})
+	if !ok {
+		return flatten
+	}
+	for _, ele := range nestedSlice {
+		switch ele.(type) {
+		case []interface{}:
+			flatten = append(flatten, Flatten(ele)...)
+		case nil:
+			continue
+		default:
+			flatten = append(flatten, ele)
+		}
+	}
+	return flatten
+
 }
