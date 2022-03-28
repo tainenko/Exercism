e8ffdf65320e46eb811cd77611716c40402b6c9d
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-03-28 22:02:27 +0800

    add solution of sorting_room.go

--- go/sorting-room/sorting_room.go
+++ go/sorting-room/sorting_room.go
@@ -1,27 +1,37 @@
 package sorting
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // DescribeNumber should return a string describing the number.
 func DescribeNumber(f float64) string {
-	panic("Please implement DescribeNumber")
+	v := strconv.FormatFloat(f, 'f', 1, 64)
+	return fmt.Sprintf("This is the number %s", v)
 }
 
+// NumberBox is an integer container
 type NumberBox interface {
 	Number() int
 }
 
 // DescribeNumberBox should return a string describing the NumberBox.
 func DescribeNumberBox(nb NumberBox) string {
-	panic("Please implement DescribeNumberBox")
+	return fmt.Sprintf("This is a box containing the number %.1f", float64(nb.Number()))
 }
 
+// FancyNumber is a number string container
 type FancyNumber struct {
 	n string
 }
 
+// Value returns the number string
 func (i FancyNumber) Value() string {
 	return i.n
 }
 
+// FancyNumberBox is a FancyNumber container
 type FancyNumberBox interface {
 	Value() string
 }
@@ -29,15 +39,33 @@ type FancyNumberBox interface {
 // ExtractFancyNumber should return the integer value for a FancyNumber
 // and 0 if any other FancyNumberBox is supplied.
 func ExtractFancyNumber(fnb FancyNumberBox) int {
-	panic("Please implement ExtractFancyNumber")
+	if _, ok := fnb.(FancyNumber); ok {
+		n, _ := strconv.Atoi(fnb.Value())
+		return n
+	}
+	return 0.0
 }
 
 // DescribeFancyNumberBox should return a string describing the FancyNumberBox.
 func DescribeFancyNumberBox(fnb FancyNumberBox) string {
-	panic("Please implement DescribeFancyNumberBox")
+	if _, ok := fnb.(FancyNumber); ok {
+		return fmt.Sprintf("This is a fancy box containing the number %.1f", float64(ExtractFancyNumber(fnb)))
+	}
+	return "This is a fancy box containing the number 0.0"
 }
 
 // DescribeAnything should return a string describing whatever it contains.
 func DescribeAnything(i interface{}) string {
-	panic("Please implement DescribeAnything")
+	switch v := i.(type) {
+	case int:
+		return fmt.Sprintf("This is the number %.1f", float64(v))
+	case float64:
+		return DescribeNumber(v)
+	case NumberBox:
+		return DescribeNumberBox(v)
+	case FancyNumberBox:
+		return DescribeFancyNumberBox(v)
+	default:
+		return "Return to sender"
+	}
 }
