e8b62ce532169071447b5b2699029c085ccc7988
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-04-05 10:27:00 +0800

    add solution of allergies.go

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,9 +1,32 @@
 package allergies
 
+var items = []string{
+	"eggs",
+	"peanuts",
+	"shellfish",
+	"strawberries",
+	"tomatoes",
+	"chocolate",
+	"pollen",
+	"cats"}
+
+// Allergies returns the full list of allergies by given allergies score
 func Allergies(allergies uint) []string {
-	panic("Please implement the Allergies function")
+	var res []string
+	for i := 0; i < len(items); i++ {
+		if allergies&(1<<i) != 0 {
+			res = append(res, items[i])
+		}
+	}
+	return res
 }
 
+// AllergicTo shows whether the given score is allergic to specific allergen
 func AllergicTo(allergies uint, allergen string) bool {
-	panic("Please implement the AllergicTo function")
+	for _, a := range Allergies(allergies) {
+		if a == allergen {
+			return true
+		}
+	}
+	return false
 }
