3b1a31dc9521a72aa5dd485f14aa6303a27f17c3
Tony Ko <31752048+tainenko@users.noreply.github.com>
2022-06-28 22:59:33 +0800

    add solution of diffle_hellman.go

--- go/diffie-hellman/diffie_hellman.go
+++ go/diffie-hellman/diffie_hellman.go
@@ -9,7 +9,7 @@ import (
 // Diffie-Hellman-Merkle key exchange
 // Private keys should be generated randomly.
 
-// PrivateKey create a private key which is greater than 1 and less than p
+// PrivateKey generate the private key which is greate than 1 and less than p
 func PrivateKey(p *big.Int) *big.Int {
 	key := new(big.Int)
 	limit := new(big.Int).Sub(p, big.NewInt(2))
@@ -17,19 +17,19 @@ func PrivateKey(p *big.Int) *big.Int {
 	return key.Rand(seed, limit).Add(key, big.NewInt(2))
 }
 
-// PublicKey calculates a public key
+// PublicKey is calculated from g**a mod p
 func PublicKey(private, p *big.Int, g int64) *big.Int {
 	return new(big.Int).Exp(big.NewInt(g), private, p)
 }
 
-// NewPair creates a key pair
-func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
-	private := PrivateKey(p)
-	public := PublicKey(private, p, g)
-	return private, public
+// NewPair create a key pair with prime p and g
+func NewPair(p *big.Int, g int64) (private, public *big.Int) {
+	private = PrivateKey(p)
+	public = PublicKey(private, p, g)
+	return
 }
 
-// SecretKey creates the secret key used for encryption.
+// SecretKey creates the secret key used for encryption
 func SecretKey(private1, public2, p *big.Int) *big.Int {
 	return new(big.Int).Exp(public2, private1, p)
 }
